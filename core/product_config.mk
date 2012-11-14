970bf05ddaedbd907f33eb26fd7a0d66af0e8a89
dman3285 <CollectiveAndroidDevs@gmail.com>
2012-11-13 19:28:36 -0500

    call aocp.mk add aocp custom updater

--- core/product_config.mk
+++ core/product_config.mk
@@ -181,13 +181,17 @@ include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
 ifneq ($(strip $(TARGET_BUILD_APPS)),)
-# An unbundled app build needs only the core product makefiles.
-all_product_configs := $(call get-product-makefiles,\
+ # An unbundled app build needs only the core product makefiles.
+ $(call import-products,$(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
+  ifneq ($(AOCP_BUILD),)
+    $(call import-products, device/*/$(AOCP_BUILD)/aocp.mk)
+  else
 # Read in all of the product definitions specified by the AndroidProducts.mk
 # files in the tree.
-all_product_configs := $(get-all-product-makefiles)
+  $(call import-products, $(get-all-product-makefiles))
+  endif
 endif
 
 # Find the product config makefile for the current product.
